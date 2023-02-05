43601f045d983babbf863fd8eb031276ca1a075a
Deem97 <thdeema@gmail.com>
2023-02-05 14:46:33 +0530

    Update project with adding composer

---
+++ src/service/BonusService.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thusara\SalaryDatesProcessor\Services;
+
+class BonusService
+{
+    // Returns the bonus payment date.
+    // If 15th of a month a weekday returns that day.
+    // If not, returns next Wednesday from 15th.
+    
+    function getBonusPaymentDate(\DateTime $date, int $month, int $year): string
+    {
+        $date->setDate($year, $month, 15);
+        $isWeekday = (int) $date->format('w');
+
+        return ($isWeekday === 0 || $isWeekday === 6)
+            ? $date->modify('next wednesday')->format('Y-m-d')
+            : $date->format('Y-m-d');
+    }
+}
---
+++ src/service/SalaryService.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thusara\SalaryDatesProcessor\Services;
+
+class SalaryService
+{
+    // Returns the salary payment date
+    // If the payment day is a weekday, return it
+    // If not, return the last weekday before the end of the month
+
+    function getSalaryPaymentDate(\DateTime $date, int $month, int $year): string
+    {
+        $date->setDate($year, $month, 1);
+        $date->modify('last day of this month');
+        $isWeekday = (int) $date->format('w');
+
+        return ($isWeekday === 0 || $isWeekday === 6)
+            ? $date->modify('last weekday')->format('Y-m-d')
+            : $date->format('Y-m-d');
+    }
+}
